2a8c39c739f7af78d79f0d3f65cf03903a4b51b0
Victor Medeiros <victorhugomls@hotmail.com>
2024-09-23 17:42:31 -0300

    config uwsgi_params and static_root env config

--- app/settings.py
+++ app/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# o static da aplicação vai estar no caminho /var/www/carros/static
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
